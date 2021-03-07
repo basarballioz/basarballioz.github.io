5d660d8a7548b60ab1d00af0e49b93bc907e8903
Basar Ballioz <balliozbasar@gmail.com>
2021-03-07 20:30:23 +0300

    project files are added

---
+++ assets/js/script.js
@@ -0,0 +1,97 @@
+//AVOID CONFLICTS BETWEEN ANOTHER LIBRARY THAT USING DOLLAR SIGN
+!(function($) {
+  "use strict";
+
+  // MAIN PAGE TYPED.JS PLUGIN
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 60,
+      backSpeed: 60,
+      backDelay: 3000
+    });
+  }
+
+  //SMOOTH SCROLL AND ACTIVATE TABS
+  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
+      let target = $(this.hash);
+      if (target.length) {
+
+        let scrollto = target.offset().top;
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1000, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        }
+        return false;
+      }
+    }
+  });
+
+  // MOBILE NAVBAR
+  $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  });
+
+  // CLOSE WHEN CLICK ON OUTSIDE
+  $(document).click(function(e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      }
+    }
+  });
+  
+  // ACTIVE STATE WHEN SCROLL
+  let nav_sections = $('section');
+  let main_nav = $('.nav-menu, .mobile-nav');
+
+  $(window).on('scroll', function() {
+    let cur_pos = $(this).scrollTop() + 400;
+
+    nav_sections.each(function() {
+      let top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
+
+  // INIT ANIMATE ON SCROLL
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
